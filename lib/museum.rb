51a5cd68ccd3d7528b670a5fa96b5ba24fd43e26
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-10 10:36:43 -0600

    expected and actaul look exactly the same - not sure why it's not passing

--- lib/museum.rb
+++ lib/museum.rb
@@ -7,7 +7,6 @@ class Museum
   end
 
   def add_exhibit(exhibit)
-
     @exhibits << exhibit
   end
 
@@ -16,8 +15,26 @@ class Museum
   end
 
   def admit(patron)
-
     @patrons << patron
+  end
 
+  def patrons_by_exhibit_interest
+    patron_interests = {}
+
+    @patrons.each do |patron|
+      patron.interests.each do |interest|
+        if patron_interests[interest]
+          patron_interests[interest] << patron
+        elsif !patron_interests[interest]
+          patron_interests[interest] = [patron]
+        end
+      end
+    end
+    @exhibits.each do |exhibit|
+    if !patron_interests.keys.include?(exhibit.name)
+      patron_interests[exhibit.name] = []
+    end
+  end
+    patron_interests
   end
 end
