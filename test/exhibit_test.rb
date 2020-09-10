86461b54618ddfdc4e982ec02ba65a83e82236ea
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-10 09:07:44 -0600

    test_it_exists and test_attributes

---
+++ test/exhibit_test.rb
@@ -0,0 +1,21 @@
+require './lib/exhibit.rb'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ExhibitTest < Minitest::Test
+
+  def test_it_exists
+
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_instance_of Exhibit, exhibit
+  end
+
+  def test_it_has_attributes
+
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
+  end
+end
